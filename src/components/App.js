3120e903e3bc71c80ed8df78604ba1cf8bf397f8
Rp-sushil <sushil.18je0861@am.iitism.ac.in>
2020-11-03 16:52:30 +0530

    react-redux-boiler-plate

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,12 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React from 'react';
 
-class App extends Component {
-    render() {
+function App() {
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  return (
+    <div id='main'>
+      
+    </div>
+  );
 }
-
 
 export default App;
